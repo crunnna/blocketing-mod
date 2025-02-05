8edbacdd149b228f81a77dcfd72235236f9e9c73
crunnna <colin.renner0@gmail.com>
2025-02-05 19:20:25 +0100

    Updating the in-game commands to make them more user-friendly

--- src/main/java/com/blocketing/ConfigurationCommand.java
+++ src/main/java/com/blocketing/ConfigurationCommand.java
@@ -21,50 +21,70 @@ public class ConfigurationCommand {
      */
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("blocketing")
-                .then(CommandManager.literal("port")
-                        .then(CommandManager.argument("port", IntegerArgumentType.integer())
+                .requires(source -> source.hasPermissionLevel(4))
+                .then(CommandManager.literal("setup")
+                        .then(CommandManager.literal("port")
+                                .then(CommandManager.argument("port", IntegerArgumentType.integer())
+                                        .executes(context -> {
+                                            int port = IntegerArgumentType.getInteger(context, "port");
+                                            ConfigLoader.setProperty("PORT", String.valueOf(port));
+                                            context.getSource().sendFeedback(() -> Text.of("Port set to " + port), true);
+                                            return 1;
+                                        })
+                                )
                                 .executes(context -> {
-                                    // Sets the port in the configuration
-                                    int port = IntegerArgumentType.getInteger(context, "port");
-                                    ConfigLoader.setProperty("PORT", String.valueOf(port));
-                                    context.getSource().sendFeedback(() -> Text.of("Port set to " + port), false);
+                                    context.getSource().sendFeedback(() -> Text.of("This command sets the port for the HTTP server."), true);
+                                    return 1;
+                                })
+                        )
+                        .then(CommandManager.literal("token")
+                                .then(CommandManager.argument("token", StringArgumentType.string())
+                                        .executes(context -> {
+                                            String token = StringArgumentType.getString(context, "token");
+                                            ConfigLoader.setProperty("BOT_TOKEN", token);
+                                            context.getSource().sendFeedback(() -> Text.of("Bot token set"), true);
+                                            return 1;
+                                        })
+                                )
+                                .executes(context -> {
+                                    context.getSource().sendFeedback(() -> Text.of("This command sets the bot token for Discord integration."), true);
+                                    return 1;
+                                })
+                        )
+                        .then(CommandManager.literal("channel")
+                                .then(CommandManager.argument("channel", StringArgumentType.string())
+                                        .executes(context -> {
+                                            String channel = StringArgumentType.getString(context, "channel");
+                                            ConfigLoader.setProperty("CHANNEL_ID", channel);
+                                            context.getSource().sendFeedback(() -> Text.of("Channel ID set"), true);
+                                            return 1;
+                                        })
+                                )
+                                .executes(context -> {
+                                    context.getSource().sendFeedback(() -> Text.of("This command sets the channel ID for Discord integration."), true);
                                     return 1;
                                 })
                         )
                 )
-                .then(CommandManager.literal("token")
-                        .then(CommandManager.argument("token", StringArgumentType.string())
-                                .executes(context -> {
-                                    // Sets the bot token in the configuration
-                                    String token = StringArgumentType.getString(context, "token");
-                                    ConfigLoader.setProperty("BOT_TOKEN", token);
-                                    context.getSource().sendFeedback(() -> Text.of("Bot token set"), false);
-                                    return 1;
-                                })
-                        )
-                )
-                .then(CommandManager.literal("channel")
-                        .then(CommandManager.argument("channel", StringArgumentType.string())
-                                .executes(context -> {
-                                    // Sets the channel ID in the configuration
-                                    String channel = StringArgumentType.getString(context, "channel");
-                                    ConfigLoader.setProperty("CHANNEL_ID", channel);
-                                    context.getSource().sendFeedback(() -> Text.of("Channel ID set"), false);
-                                    return 1;
-                                })
-                        )
-                )
+
                 .then(CommandManager.literal("toggle")
                         .then(CommandManager.literal("advancements")
                                 .executes(context -> {
-                                    // Toggles the advancementsEnabled flag
                                     MinecraftChatHandler.toggleAdvancementsEnabled();
                                     boolean status = MinecraftChatHandler.isAdvancementsEnabled();
-                                    context.getSource().sendFeedback(() -> Text.of("Advancements " + (status ? "enabled" : "disabled")), false);
+                                    context.getSource().sendFeedback(() -> Text.of("Advancements " + (status ? "enabled" : "disabled")), true);
                                     return 1;
                                 })
                         )
+                        .executes(context -> {
+                            context.getSource().sendFeedback(() -> Text.of("This command toggles the sending of advancement messages to Discord."), true);
+                            return 1;
+                        })
                 )
+                .executes(context -> {
+                    context.getSource().sendFeedback(() -> Text.of("Available commands: setup, toggle"), true);
+                    return 1;
+                })
         );
     }
 }
